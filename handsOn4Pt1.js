39b1283f3036a7ab0fdbaeab2840f17f81d63d72
pgreenfield99 <70538405+pgreenfield99@users.noreply.github.com>
2020-09-28 18:19:08 -0500

    Add files via upload

---
+++ handsOn4Pt1.js
@@ -0,0 +1,33 @@
+var $ = function (id) 
+{
+    return document.getElementById(id);
+}
+
+
+ var calculate = function()
+ {
+   var floatMonthlyPayment;
+   var intLoanTerm;
+   var floatTotalCost;
+   
+ 
+   floatMonthlyPayment = parseFloat($("monthly_payment").value);
+   intLoanTerm = parseInt($("loan_length").value);
+   floatTotalCost = parseFloat($("total_cost").value);
+    floatTotalCost = floatMonthlyPayment * intLoanTerm;
+    $("total_cost").value= floatTotalCost;
+       
+    
+ }
+ 
+ window.onload = function ()
+ 
+{
+    $("loan_length").value = "";
+    $("total_cost").value = "";
+    $("calc").onclick = calculate;
+    $("monthly_payment").focus();
+}
+
+
+ 
